a230875188cca16ff94ea7a5d0dd3485a722855a
amiya.rout <amiya.rout@globallogic.com>
2021-10-18 19:44:11 +0530

    Initial commit

---
+++ cloud-gateway/src/test/java/com/globallogic/cloud/gateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globallogic.cloud.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ user-service/src/main/java/com/globallogic/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.globallogic.user.repository;
+
+import com.globallogic.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUserId(Long userId);
+}
